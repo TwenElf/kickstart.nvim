dfa99bd81df0701a5e91cb228b0a38643a80ce04
Twenne Elffers <twenne.elffers@hva.nl>
2024-09-17 01:19:34 +0200

    Set colorscheme to darcula from intellij

--- init.lua
+++ init.lua
@@ -823,28 +823,39 @@ require('lazy').setup({
       }
     end,
   },
+  { -- colorschemes
+    'rktjmp/lush.nvim',
+    priority = 1001,
+  },
+  {
+    'santos-gabriel-dario/darcula-solid.nvim',
+    priority = 1002,
+  },
   { -- You can easily change to a different colorscheme.
     -- Change the name of the colorscheme plugin below, and then
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     'catppuccin/nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
+    priority = 1003, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'catppuccin-macchiato'
+      vim.cmd.colorscheme 'darcula-solid'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
     end,
   },
-
-  -- Highlight TODO  , notes, etc in comments event = 'VimEnter', 
-  { 'folke/todo-comments.nvim',lazy=false, dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = true }, 
+  -- Highlight TODO  , notes, etc in comments event = 'VimEnter',
+  {
+    'folke/todo-comments.nvim',
+    lazy = false,
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = { signs = true },
     config = function()
-        require("todo-comments").setup()
+      require('todo-comments').setup()
     end,
   },
 
